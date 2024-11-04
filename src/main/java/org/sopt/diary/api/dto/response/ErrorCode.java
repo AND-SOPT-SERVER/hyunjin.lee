3b3cdc0bce30bd7b5f47795bdd270403347b7431
2hyunjinn <huenjin04@naver.com>
2024-11-04 14:47:11 +0900

    [chore] #6 회원가입 관련 에러코드 추가

--- src/main/java/org/sopt/diary/api/dto/response/ErrorCode.java
+++ src/main/java/org/sopt/diary/api/dto/response/ErrorCode.java
@@ -1,9 +1,13 @@
 package org.sopt.diary.api.dto.response;
 
 public enum ErrorCode {
-    INVALID_INPUT_VALUE(40000, "유효하지 않은 요청입니다."),
+    INVALID_INPUT_VALUE(40001, "유효하지 않은 요청입니다."),
     INVALID_INPUT_TITLE_DUPLICATE(40003, "이미 존재하는 제목입니다."),
     DIARY_NOT_FOUND(40004, "존재하지 않는 다이어리입니다."),
+    USER_NOT_FOUND(40005, "존재하지 않는 사용자입니다."),
+    USERNAME_ALREADY_EXISTS(40900, "이미 존재하는 아이디입니다."),
+    NICKNAME_ALREADY_EXISTS(40901, "이미 존재하는 닉네임입니다."),
+    INVALID_PASSWORD(40100, "비밀번호가 일치하지 않습니다."),
     INTERNAL_SERVER_ERROR(50000, "서버 내부 오류입니다.");
 
     private final int code;
@@ -14,11 +18,6 @@ public enum ErrorCode {
         this.message = message;
     }
 
-    public int getCode() {
-        return code;
-    }
-
-    public String getMessage() {
-        return message;
-    }
+    public int getCode() { return code; }
+    public String getMessage() { return message; }
 }
